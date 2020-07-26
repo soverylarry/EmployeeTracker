cd31530116dd51c66fe826f37407221188880c9e
larry laconi <soverylarry@gmail.com>
2020-07-26 18:07:14 -0400

    first commit of initial files

---
+++ database/app.js
@@ -0,0 +1,6 @@
+//write connection to our database. This is where our 
+//SQL queries will reside
+
+//findAllemployee findAllManagers, find AllDept's etc.
+
+//export to one level up 
---
+++ index.js
@@ -0,0 +1,7 @@
+
+//Const's will reside here
+//this will be the looooonnnggg file.  all of my Inquirer questions..
+// Init function
+//branching and Switch functions, tree out all the different branches.
+
+// big Switch case in the middle
